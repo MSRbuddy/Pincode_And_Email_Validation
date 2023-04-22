1cf962f0367a1ee002cd0ebd0bcef205a9491120
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 11:53:19 +0530

    Added : Pincode Validation Program UC-2!

---
+++ PinCode-UC2/PinCode-UC2/PincodeValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCode_UC2
+{
+    internal class PincodeValidator
+    {
+        public static string PIN_REGEXUC2 = "^(?![a-zA-Z#$%&'*+/=?^_`{|}~-][0-9]).{6}$";
+
+        public bool maximumDigitValidator(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, PIN_REGEXUC2);
+        }
+    }
+}
