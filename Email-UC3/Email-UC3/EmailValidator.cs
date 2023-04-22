05c30463589c40ad10ea2953902ede278621238f
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:01:03 +0530

    Added : Email Validation Program UC-3!

---
+++ Email-UC3/Email-UC3/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Email_UC3
+{
+    internal class EmailValidator
+    {
+        public static string EMAIL_REGEX = "^[0-9A-Za-z]+([+-_.][a-zA-Z]+)*[@][0-9A-Za-z]+$";
+
+        public bool emailIDValidator(string emailId)
+        {
+            return Regex.IsMatch(emailId, EMAIL_REGEX);
+        }
+    }
+}
---
+++ Email-UC3/Email-UC3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Email_UC3
+{
+    internal class Program
+    {
+        public static string emailId;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to the Pin Code and Email Validator*****");
+            Console.WriteLine("---------------------------------------------------------\n");
+
+            EmailValidator EmailRegex = new EmailValidator();
+            bool validationKey;
+            Console.WriteLine("\nEnter the Email-ID : ");
+            emailId = Console.ReadLine();
+            validationKey = EmailRegex.emailIDValidator(emailId);
+
+            Console.WriteLine(validationKey ? "\nThe Email Id {0} is Valid." : "\nThe Email Id {0} is Not Valid.", emailId);
+        }
+    }
+}
