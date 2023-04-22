6bbfe48168787eb0ddaa2ebf9a5bfd2ca15f5991
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 11:56:04 +0530

    Added : Pincode Validation Program UC-3!

---
+++ PinCode-UC3/PinCode-UC3/PincodeValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCode_UC3
+{
+    internal class PincodeValidator
+    {
+        public static string PIN_REGEXUC3 = "^((?![a-zA-Z#$%&'*+/=?^_`{|}~-])[0-9](?![a-zA-Z#$%&'*+/=?^_`{|}~-])){6}$";    
+
+        public bool maximumDigitValidator(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, PIN_REGEXUC3);
+        }
+    }
+}
---
+++ PinCode-UC3/PinCode-UC3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PinCode_UC3
+{
+    internal class Program
+    {
+        public static string pinCode;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to the Pin Code and Email Validator*****");
+            Console.WriteLine("--------------------------------------------------------\n");
+            
+            PincodeValidator pinCodeRegex = new PincodeValidator();
+            bool validationKey;
+            Console.WriteLine("\nEnter the pin code : ");
+            pinCode = Console.ReadLine();
+            validationKey = pinCodeRegex.maximumDigitValidator(pinCode);
+
+            Console.WriteLine(validationKey ? "\nThe Pin Code {0} is Valid." : "\nThe Pin Code {0} is Not Valid.", pinCode);
+        }
+    }
+}
