9d4ed3b2bfbadb7a8c28494aefde0baab34674ba
MSRbuddy <122597457+MSRbuddy@users.noreply.github.com>
2023-04-22 12:09:24 +0530

    Update EmailValidator.cs

--- Email-UC2/Email-UC2/EmailValidator.cs
+++ Email-UC2/Email-UC2/EmailValidator.cs
@@ -4,7 +4,7 @@ namespace Email_UC2
 {
     internal class EmailValidator
     {
-        public static string EMAIL_REGEX = "^[0-9A-Za-z]+([+-_.][a-zA-Z]+)*$";
+        public static string EMAIL_REGEX = "^[0-9A-Za-z]+[@]+([+-_.][a-zA-Z]+)*$";
         public bool emailIDValidator(string emailId)
         {
             return Regex.IsMatch(emailId, EMAIL_REGEX);
