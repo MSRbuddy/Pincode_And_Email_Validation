b969c7c52e0c976dc8e74a88fbd9692af3acc2c6
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 11:59:11 +0530

    Added : Email Validation Program UC-1!

---
+++ Email-UC1/Email-UC1/EmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Email_UC1
+{
+    internal class EmailValidator
+    {
+        public static string EMAIL_REGEX = "^[0-9A-Za-z]$";
+        public bool emailIDValidator(string emailId)
+        {
+            return Regex.IsMatch(emailId, EMAIL_REGEX);
+        }
+    }
+}
