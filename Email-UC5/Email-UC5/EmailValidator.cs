7b476c9fc54988070d0a60e48392458086f299c6
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:03:01 +0530

    Added : Email Validation Program UC-5!

---
+++ Email-UC5/Email-UC5/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Email_UC5
+{
+    internal class EmailValidator
+    {
+        public static string EMAIL_REGEX = "^[0-9A-Za-z]+([+-_.][a-zA-Z]+)*[@][0-9A-Za-z]+[.][0-9A-Za-z]{2,3}([.][A-Za-z]{2})$";
+        
+        public bool emailIDValidator(string emailId)
+        {
+            return Regex.IsMatch(emailId, EMAIL_REGEX);
+        }
+    }
+}
