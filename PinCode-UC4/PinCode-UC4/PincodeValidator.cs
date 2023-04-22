416be6f215c5e2a78c0b9475e2bca238de5903c3
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 11:57:47 +0530

    Added : Pincode Validation Program UC-4!

---
+++ PinCode-UC4/PinCode-UC4/PincodeValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCode_UC4
+{
+    internal class PincodeValidator
+    {
+        public static string PIN_REGEXUC4 = "^[0-9]{3}[ ]?[0-9]{3}$";
+        
+        public bool maximumDigitValidator(string pinCode)
+        {
+            return Regex.IsMatch(pinCode, PIN_REGEXUC4);
+        }
+    }
+}
